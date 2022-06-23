04cb9c3e0f92b0831363de029e3f1ccf347e674e
Nowosad <tupiszakaczki@gmail.com>
2022-06-23 19:10:05 -0400

    adds ts code

---
+++ R/time-series_data/climate_dtw.R
@@ -0,0 +1,47 @@
+library(terra)
+library(sf)
+library(supercells)
+library(rgeoda)
+library(purrr)
+library(tmap)
+get_dtw2d = function(x){
+  dist_mat = matrix(nrow = nrow(x), ncol = nrow(x))
+  for (i in seq_len(nrow(x))){
+    mat1 = matrix(unlist(x[i, ]), ncol = 2)
+    for (j in seq_len(nrow(x))){
+      mat2 = matrix(unlist(x[j, ]), ncol = 2)
+      dist_mat[i, j] = dtwclust::dtw_basic(mat1, mat2, norm = "L2", step.pattern = dtw::symmetric2)
+    }
+  }
+  stats::as.dist(dist_mat)
+}
+dtw_2d = function(x, y){
+  dtw2ddistance = dtwclust::dtw_basic(x = matrix(x, ncol = 2), y = matrix(y, ncol = 2),
+                                      norm = "L2", step.pattern = dtw::symmetric2,
+                                      error.check = FALSE)
+  return(dtw2ddistance)
+}
+regionalize_dtw_2d = function(k, superpixels, ...){
+  weight_df = st_drop_geometry(superpixels[, !colnames(superpixels) %in% c("supercells", "x", "y")])
+  weight_dist = get_dtw2d(weight_df)
+  rook_w = rook_weights(superpixels)
+  skater_results = skater(k, rook_w, weight_df, random_seed = 1, cpu_threads = 1, scale_method = "raw",
+                          rdist = weight_dist)
+  superpixels$cluster = skater_results$Clusters
+  regions = aggregate(superpixels, by = list(superpixels$cluster), mean)
+  regions = st_cast(regions, "POLYGON")
+  regions$k = k
+  return(regions)
+}
+ta = rast("raw-data/ta_scaled.tif")
+pr = rast("raw-data/pr_scaled.tif")
+
+clim = c(ta, pr)
+
+sp = supercells(clim, step = 15, compactness = 0.01, dist_fun = dtw_2d)
+
+sp_regions = map_dfr(c(3, 7, 11, 15), regionalize_dtw_2d, sp)
+
+tm_shape(sp_regions) +
+  tm_polygons() +
+  tm_facets("k")
---
+++ R/time-series_data/climate_euc.R
@@ -0,0 +1,49 @@
+library(terra)
+library(sf)
+library(supercells)
+library(rgeoda)
+library(purrr)
+library(tmap)
+scale_01_indepenent = function(x, min_x, max_x){
+  (x - min(x, na.rm = TRUE)) / (max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
+}
+rast_to_LAB = function(x) {
+  x_vals = as.data.frame(x, na.rm = FALSE)
+  x2_vals = apply(x_vals, 2, scale_01_indepenent)
+  x2_vals = grDevices::convertColor(x2_vals, from = "sRGB", to = "Lab")
+  x2 = x
+  values(x2) = x2_vals
+  return(x2)
+}
+regionalize_euc = function(k, superpixels, ...){
+  weight_df = st_drop_geometry(superpixels[, !colnames(superpixels) %in% c("supercells", "x", "y")])
+  rook_w = rook_weights(superpixels)
+  skater_results = skater(k, rook_w, weight_df, random_seed = 1, cpu_threads = 1, scale_method = "raw")
+  superpixels$cluster = skater_results$Clusters
+  regions = aggregate(superpixels, by = list(superpixels$cluster), mean)
+  regions = st_cast(regions, "POLYGON")
+  regions$k = k
+  return(regions)
+}
+
+ta = rast("raw-data/ta_scaled.tif")
+pr = rast("raw-data/pr_scaled.tif")
+clim = c(ta, pr)
+
+# pca ------------------------------------------------------------------
+ta_pca = prcomp(as.data.frame(ta))
+ta_pca_rast = predict(ta, ta_pca)
+pr_pca = prcomp(as.data.frame(pr))
+pr_pca_rast = predict(pr, pr_pca)
+all_rast = c(ta_pca_rast[[1:3]], pr_pca_rast[[1:3]])
+names(all_rast) = LETTERS[1:6]
+all_pca = prcomp(as.data.frame(all_rast))
+all_pca3_rast = predict(all_rast, all_pca)[[1:3]]
+all_pca3_rast_lab = rast_to_LAB(all_pca3_rast)
+sp = supercells(all_pca3_rast_lab, step = 15, compactness = 0.05)
+
+sp_regions = map_dfr(c(3, 7, 11, 15), regionalize_euc, sp)
+
+tm_shape(sp_regions) +
+  tm_polygons() +
+  tm_facets("k")
