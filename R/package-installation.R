07f85709a76c4c7c546e96d996cb379f5565073f
Jakub Nowosad <tupiszakaczki@gmail.com>
2022-08-31 09:14:09 +0200

    closes #1

--- R/package-installation.R
+++ R/package-installation.R
@@ -1,4 +1,4 @@
 install.packages("remotes")
-pkgs = c("supercells", "terra", "sf",
+pkgs = c("supercells", "terra", "sf", "regional",
          "motif", "tmap", "purrr", "rgeoda", "dtwclust")
 remotes::install_cran(pkgs)
